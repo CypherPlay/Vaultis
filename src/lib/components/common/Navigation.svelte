239383cc039ca87319c342b4adacc949777e6e21
Helga Idun <helgaidun@gmail.com>
2025-10-16 07:02:21 +0000

    feat: Add responsive navigation menu component

--- src/lib/components/common/Navigation.svelte
+++
@@ -1,7 +0,0 @@
-<nav class="bg-gray-700 p-4">
-  <ul class="flex space-x-4 text-white">
-    <li><a href="/">Home</a></li>
-    <li><a href="/about">About</a></li>
-    <li><a href="/contact">Contact</a></li>
-  </ul>
-</nav>
