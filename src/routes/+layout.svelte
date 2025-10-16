d9ec24e2b23e8506ab16bb33897c473614a56772
Adani Masuk <adanimasuk01@gmail.com>
2025-10-16 03:48:04 +0000

    Initialize SvelteKit project with modular structure and Tailwind CSS

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import MainLayout from '$lib/layouts/MainLayout.svelte';
 
 	let { children } = $props();
 </script>
@@ -9,4 +10,6 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children?.()}
+<MainLayout>
+	{@render children?.()}
+</MainLayout>
