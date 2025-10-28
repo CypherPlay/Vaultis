c45b42faddec3e9e5f7be566b514f1a43d73b70c
Georgine Forner <jackson.richard.btc@gmail.com>
2025-10-28 09:06:36 +0000

    feat: Add daily riddle page

---
+++ src/routes/riddle/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import MainLayout from '../../lib/layouts/MainLayout.svelte';
+  import DailyRiddleDisplay from '../../lib/components/riddle/DailyRiddleDisplay.svelte';
+  import GuessSubmission from '../../lib/components/riddle/GuessSubmission.svelte';
+</script>
+
+<MainLayout>
+  <div class="flex flex-col items-center justify-center min-h-screen p-4">
+    <div class="w-full max-w-md mb-8">
+      <DailyRiddleDisplay />
+    </div>
+    <div class="w-full max-w-md">
+      <GuessSubmission />
+    </div>
+  </div>
+</MainLayout>
