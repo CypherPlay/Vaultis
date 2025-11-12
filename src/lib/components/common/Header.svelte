cde7b16a05b419062f152afaff3b6beb08cbc92b
Arjen Hammond <arjenhammond@gmail.com>
2025-11-12 06:00:05 -0700

    Feat: Display riddle countdown on leaderboard page (#32)
    * Add countdown timer to leaderboard header
    
    * fix: Automatically update riddle unlock time in header

--- src/lib/components/common/Header.svelte
+++ src/lib/components/common/Header.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
 	import WalletConnect from '$lib/components/wallet/WalletConnect.svelte';
+	import CountdownTimer from '$lib/components/riddle/CountdownTimer.svelte';
 	import { walletStore } from '$lib/stores/walletStore';
 
 	export let title: string = 'Vaultis'; // Default title
 
 	let isConnected: boolean;
 	let walletAddress: string | null;
+	let nextRiddleUnlockTime: Date = calculateNextRiddleUnlockTime();
+
+	function calculateNextRiddleUnlockTime(): Date {
+		const now = new Date();
+		const nextUnlock = new Date(
+			Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate() + 1, 0, 0, 0, 0)
+		);
+		return nextUnlock;
+	}
 
 	walletStore.subscribe((state) => {
 		isConnected = state.isConnected;
@@ -33,6 +43,9 @@
 			class="mt-4 md:mt-0 md:flex md:w-auto md:items-center md:text-left md:flex-grow block w-full flex-grow text-center"
 		>
 			<h1 class="text-2xl font-bold">{title}</h1>
+			<div class="md:ml-4 mt-2 md:mt-0">
+				<CountdownTimer endTimestamp={nextRiddleUnlockTime.getTime()} on:riddleExpired={() => (nextRiddleUnlockTime = calculateNextRiddleUnlockTime())} />
+			</div>
 		</div>
 
 		<!-- Wallet Connect Button -->
